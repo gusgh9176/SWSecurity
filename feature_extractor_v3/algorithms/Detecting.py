eb88abcd526464ff1a5fc3aa5b619326359fc081
gusgh9176 <gusgh9176@naver.com>
2019-05-04 14:42:25 +0900

    copy app

--- feature_extractor_v3/algorithms/Detecting.py
+++ feature_extractor_v3/algorithms/Detecting.py
@@ -13,18 +13,21 @@ import json
 import subprocess, os
 import re
 import operator
+import shutil #추가한코드
 
 # return True -> malware
 # return False -> benign
 malware_Permission = ['CHANGE_NETWORK_STATE', 'UNINSTALL_SHORTCUT', 'WRITE_APN_SETTINGS', 'ACCESS_COARSE_UPDATES', 'DEVICE_POWER', 'PROCESS_OUTGOING_CALLS', 'INSTALL_SHORTCUT', 'CAMERA, WRITE_OWNER_DATA', 'REORDER_TASKS', 'WRITE_SETTINGS', 'DELETE_PACKAGES', 'ACCESS_GPS', 'READ_SETTINGS', 'CHECK_LICENSE', 'BROADCAST_PACKAGE_REMOVED', 'GET_PACKAGE_SIZE', 'INSTALL_PACKAGES', 'ACCESS_LOCATION', 'READ_SETTINGS', 'WRITE_SETTINGS', 'PERMISSION_NAME', 'READ_OWNER_DATA', 'FLASHLIGHT', 'DELETE_CACHE_FILES', 'CLEAR_APP_CACHE', 'MOUNT_UNMOUNT_FILESYSTEMS', 'RESTART_PACKAGES']
-def classify(Permission_final):
+def classify(Permission_final, filename):
     for Pm in Permission_final:
         for mal_pm in malware_Permission:
             a = str(Pm).find(mal_pm)
             if(a != -1):
                 print("This is Malware!!!!!!!!!!!!!!1")
+				shutil.copy(filename, 'classify/malware')
                 return True
     print("This is benign~~~~~~~~~~~~~~~")
+	shutil.copy(filename, 'classify/benign')
     return False
 
 
